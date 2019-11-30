0df4ab503ee12184774351e773f4f912cac99de4
s1mpleboy <648432889@qq.com>
2019-11-30 17:02:29 +0800

    Revert "忽略文件和 vscode 配置文件修订"
    This reverts commit 40c6f928b9df9304c9d4f0e5848efed71ce5188d.

--- 点亮LED/3rdPartCode/C51程序参考大全/150-综合测试程序/timer.h
+++
@@ -1,8 +0,0 @@
-#include<reg52.h>
-
-#ifndef __DISPLAY_H__
-#define __DISPLAY_H__
-
-void Init_Timer0(void);
-
-#endif
--- 点亮LED/prj1/code(5278)/inc/delay.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DELAY_H_
-#define __DELAY_H_
-
-#define u8 unsigned char
-#define u16 unsigned int
-
-void delayms_lo(u16 ms);
-void delayms_sh(u8 ms);	
-#endif
